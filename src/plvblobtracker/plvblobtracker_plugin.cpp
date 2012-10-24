8393291ed115d621eb4f43fd805a76c41ded5c87
Roberdus <robbyvd@gmail.com>
2012-10-24 17:37:32 +0200

    fix sloppy commit of removal Nao and LED trackers

--- src/plvblobtracker/plvblobtracker_plugin.cpp
+++ src/plvblobtracker/plvblobtracker_plugin.cpp
@@ -26,11 +26,10 @@
 
 #include "BlobDetector.h"
 #include "BlobTracker.h"
-#include "LedBlobTracker.h"
 #include "VPBlobToStringConverter.h"
 #include "TrackToStringConverter.h"
 #include "TouchBlobAndStringConverter.h"
-#include "NaoBlob.h"
+
 
 using namespace plv;
 
@@ -48,12 +47,10 @@ void PlvBlobTrackPlugin::onLoad()
     qRegisterMetaType< QList<plvblobtracker::Blob*> >("QList<plvopencv::Blob*>");
 
     plvRegisterPipelineElement<plvblobtracker::BlobTracker>();
-	plvRegisterPipelineElement<plvblobtracker::LedBlobTracker>();
     plvRegisterPipelineElement<plvblobtracker::BlobDetector>();
     plvRegisterPipelineElement<plvblobtracker::VPBlobToStringConverter>();
 	plvRegisterPipelineElement<plvblobtracker::TrackToStringConverter>();
 	plvRegisterPipelineElement<plvblobtracker::TouchBlobAndStringConverter>();
-	plvRegisterPipelineElement<plvblobtracker::NaoBlob>();
 
 }
 
