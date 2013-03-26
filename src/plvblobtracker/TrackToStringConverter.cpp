adf5f9e99204ca05821332a4af19302412f76886
Roberdus <robbyvd@gmail.com>
2013-03-27 00:00:59 +0100

    option to save the blobtracks and ages to fixed textfile(for now

--- src/plvblobtracker/TrackToStringConverter.cpp
+++ src/plvblobtracker/TrackToStringConverter.cpp
@@ -4,8 +4,12 @@ using namespace plv;
 using namespace plvblobtracker;
 
 #include <opencv/cv.h>
+#include <QFile>
 
-TrackToStringConverter::TrackToStringConverter()
+TrackToStringConverter::TrackToStringConverter() :
+	m_saveToFile(true),
+	m_filename("blobtracklog.txt"),
+	halfhour(0)
 {
     m_inputBlobs = createInputPin< QList<BlobTrack> >( "input", this );
   
@@ -17,12 +21,72 @@ TrackToStringConverter::~TrackToStringConverter()
 {
 }
 
+bool TrackToStringConverter::init()
+{
+	//m_timenotset = true;
+	if (getSaveToFile())
+	{
+		QFile file(m_filename);
+		bool ret = file.open(QIODevice::WriteOnly | QIODevice::Append);
+		Q_ASSERT(ret);
+		QTextStream s(&file);
+		QString dateAndTime = QTime().currentTime().toString(); //QString("blobs of frame %1").arg(this->getProcessingSerial());
+		file.write("\n");
+		file.write("init time:");
+		for (int i = 0; i < dateAndTime.size(); ++i)
+			s << dateAndTime.at(i);// << '\n';
+		file.write("\n");	
+		ret = file.flush();
+		Q_ASSERT(ret);
+		file.close();
+		//m_timenotset = false;
+	}
+	return true;
+}
+
+void TrackToStringConverter::setSaveToFile(bool b)
+{
+	m_saveToFile = b;
+	//if (b)
+	//{
+	//	QFile file(m_filename);
+	//	bool ret = file.open(QIODevice::WriteOnly | QIODevice::Append);
+	//	Q_ASSERT(ret);
+	//	QTextStream s(&file);
+	//	file.write("set to save time:");
+	//	QString dateAndTime = QTime().currentTime().toString(); //QString("blobs of frame %1").arg(this->getProcessingSerial());
+	//	for (int i = 0; i < dateAndTime.size(); ++i)
+	//		s << dateAndTime.at(i);// << '\n';
+	//	file.write("\n");	
+	//	ret = file.flush();
+	//	Q_ASSERT(ret);
+	//	file.close();
+	//	//m_timenotset = false;
+	//}
+	emit (saveToFileChanged(b));
+}
+
 bool TrackToStringConverter::process()
 {
     QList<plvblobtracker::BlobTrack> tracks = m_inputBlobs->get();
-
+	bool savetofile = true;
     QString out = QString("FRAME:%1#").arg( this->getProcessingSerial() );
 
+	if (getSaveToFile())
+	{
+			QFile file(m_filename);
+			bool ret = file.open(QIODevice::WriteOnly | QIODevice::Append);
+			Q_ASSERT(ret);
+			QTextStream s(&file);
+			QString dateAndTime = QTime().currentTime().toString();
+			QString frameNumber = QString("FRAME:%1#, at time %2").arg(this->getProcessingSerial()).arg(dateAndTime);;
+			for (int i = 0; i < frameNumber.size(); ++i)
+				s << frameNumber.at(i);// << '\n';
+			file.write("\n");	
+			ret = file.flush();
+			Q_ASSERT(ret);
+			file.close();
+	}
     // convert coordinates to what the virtual playground expects
     foreach( BlobTrack t , tracks )
     {
@@ -30,12 +94,53 @@ bool TrackToStringConverter::process()
         //p.x = image.width() - p.x;
         //p.y = image.height() - p.y;
 		//playgroundQString blobString = QString("BEGIN_MARKER#MARKER_ID:%1#MARKER_CENTER_X:%2#MARKER_CENTER_Y:%3#").arg(t.getID()).arg(p.x).arg(p.y);
-		QString blobString = QString("BEGIN_MARKER#MARKER_ID:%1#MARKER_CENTER_X:%2#MARKER_CENTER_Y:%3#DIRECTION:%4#SPEED:%5#AVERAGEZ:%6").arg(t.getId()).arg(p.x).arg(p.y).arg(t.getAvgDirection()).arg(t.getAvgVelocity()).arg(t.getAveragePixel());
+		QString blobString = QString("BEGIN_MARKER#MARKER_ID:%1#MARKER_CENTER_X:%2#MARKER_CENTER_Y:%3#DIRECTION:%4#SPEED:%5#AVERAGEZ:%6#AGE:%7").arg(t.getId()).arg(p.x).arg(p.y).arg(t.getAvgDirection()).arg(t.getAvgVelocity()).arg(t.getAveragePixel()).arg(t.getAge());
 		out.append(blobString);
 		out.append("\n");
+		if (getSaveToFile())
+		{
+			QFile file(m_filename);
+			bool ret = file.open(QIODevice::WriteOnly | QIODevice::Append);
+			Q_ASSERT(ret);
+			//Q_ASSERT(blobString.size()>0);
+			QTextStream s(&file);
+			for (int i = 0; i < blobString.size(); ++i)
+				s << blobString.at(i);// << '\n';
+			//file.write(blobString);
+			//file.write("APPEND new Line");
+			file.write("\n");	
+			ret = file.flush();
+			Q_ASSERT(ret);
+			file.close();
+		}
     }
 
-    // end TCP frame with newline
+	//if (getSaveToFile())
+	//{
+	//	if (halfhour>54000)
+	//	//if (halfhour>60)
+	//	{
+	//		halfhour = 0;
+	//		QFile file(m_filename);
+	//		bool ret = file.open(QIODevice::WriteOnly | QIODevice::Append);
+	//		Q_ASSERT(ret);
+	//		QTextStream s(&file);
+	//		QString dateAndTime = QTime().currentTime().toString(); //QString("blobs of frame %1").arg(this->getProcessingSerial());
+	//		file.write("current time:");
+	//		for (int i = 0; i < dateAndTime.size(); ++i)
+	//			s << dateAndTime.at(i);// << '\n';
+	//		file.write("\n");	
+	//		ret = file.flush();
+	//		Q_ASSERT(ret);
+	//		file.close();
+	//		//m_timenotset = false;
+	//	}
+	//	else
+	//	{
+	//		halfhour++;
+	//	}
+	//}
+    // end TCP frame with newline, why?
     out.append("\n");
 
     m_outputPin->put(out);
--- src/plvblobtracker/TrackToStringConverter.h
+++ src/plvblobtracker/TrackToStringConverter.h
@@ -18,16 +18,29 @@ class TrackToStringConverter : public plv::PipelineProcessor
     Q_CLASSINFO("name", "Track to String Converter")
     Q_CLASSINFO("description", "A processor which converts a track list to a string with it ids,position,direction and velocity.")
 
+	Q_PROPERTY( bool saveToFile READ getSaveToFile WRITE setSaveToFile NOTIFY saveToFileChanged )
+
 public:
     TrackToStringConverter();
     virtual ~TrackToStringConverter();
+	virtual bool init();
+	bool getSaveToFile() {return m_saveToFile;}
 
     /** required standard method declaration for plv::PipelineProcessor */
     PLV_PIPELINE_PROCESSOR
+signals:
+	void saveToFileChanged (bool b);
+
+public slots:
+	void setSaveToFile(bool b);
 
 private:
     plv::InputPin< QList<plvblobtracker::BlobTrack> >* m_inputBlobs;
     plv::OutputPin<QString>* m_outputPin;
+	bool m_saveToFile;
+	unsigned short halfhour; 
+	//bool m_timenotset;
+	QString m_filename;
 };
 
 }
