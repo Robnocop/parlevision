e76ca1b3c2547b2c09cbb262c61135feafe6ac9e
Roberdus <robbyvd@gmail.com>
2013-03-27 00:22:35 +0100

    tab delimited format

--- src/plvblobtracker/TrackToStringConverter.cpp
+++ src/plvblobtracker/TrackToStringConverter.cpp
@@ -32,6 +32,10 @@ bool TrackToStringConverter::init()
 		QTextStream s(&file);
 		QString dateAndTime = QTime().currentTime().toString(); //QString("blobs of frame %1").arg(this->getProcessingSerial());
 		file.write("\n");
+		file.write("tabbed format:"); 
+		file.write("\n");
+		file.write("BEGIN_MARKER#MARKER_ID: \t #MARKER_CENTER_X: \t #MARKER_CENTER_Y: \t #DIRECTION: \t #SPEED: \t #AVERAGEZ: \t #AGE:");
+		file.write("\n");
 		file.write("init time:");
 		for (int i = 0; i < dateAndTime.size(); ++i)
 			s << dateAndTime.at(i);// << '\n';
@@ -79,7 +83,7 @@ bool TrackToStringConverter::process()
 			Q_ASSERT(ret);
 			QTextStream s(&file);
 			QString dateAndTime = QTime().currentTime().toString();
-			QString frameNumber = QString("FRAME:%1#, at time %2").arg(this->getProcessingSerial()).arg(dateAndTime);;
+			QString frameNumber = QString("FRAME: \t %1 \t at time \t %2").arg(this->getProcessingSerial()).arg(dateAndTime);;
 			for (int i = 0; i < frameNumber.size(); ++i)
 				s << frameNumber.at(i);// << '\n';
 			file.write("\n");	
@@ -94,7 +98,7 @@ bool TrackToStringConverter::process()
         //p.x = image.width() - p.x;
         //p.y = image.height() - p.y;
 		//playgroundQString blobString = QString("BEGIN_MARKER#MARKER_ID:%1#MARKER_CENTER_X:%2#MARKER_CENTER_Y:%3#").arg(t.getID()).arg(p.x).arg(p.y);
-		QString blobString = QString("BEGIN_MARKER#MARKER_ID:%1#MARKER_CENTER_X:%2#MARKER_CENTER_Y:%3#DIRECTION:%4#SPEED:%5#AVERAGEZ:%6#AGE:%7").arg(t.getId()).arg(p.x).arg(p.y).arg(t.getAvgDirection()).arg(t.getAvgVelocity()).arg(t.getAveragePixel()).arg(t.getAge());
+		QString blobString = QString("%1 \t %2 \t %3 \t %4 \t %5 \t %6 \t %7").arg(t.getId()).arg(p.x).arg(p.y).arg(t.getAvgDirection()).arg(t.getAvgVelocity()).arg(t.getAveragePixel()).arg(t.getAge());
 		out.append(blobString);
 		out.append("\n");
 		if (getSaveToFile())
